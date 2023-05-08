8e14bfdb750a5ad05d7a13c69070398225a82eb0
rajusharma0202 <132940110+rajusharma0202@users.noreply.github.com>
2023-05-09 00:02:06 +0530

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('MY Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 3 & Blueberry Oatmeal)
+streamlit.text('Omega3 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
