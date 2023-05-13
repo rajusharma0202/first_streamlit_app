a00095e1afd7d67affb9b334e2da8602704b705f
rajusharma0202 <132940110+rajusharma0202@users.noreply.github.com>
2023-05-13 17:17:53 +0530

    Update streamlit_app.py
    add try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,15 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice
+streamlit.error("Please select a fruit to get information")
+else
+#streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response.json())
 
@@ -35,7 +40,8 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
 streamlit.stop()
 
 #import snowflake.connector
